259dac24a786136b2b99fd3fe75dea13c7e4875c
Keli06001 <keli06001@utb.vaxjo.se>
2024-02-02 12:25:18 +0100

    Add project files.

---
+++ ILU7_1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace ILU7_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> list = new List<String>();
+
+            Console.WriteLine("Skriv in så många namn som du vill, när du känner dig klar så skickar du en tom rad");
+            string namn = " ";
+            while (namn!="") 
+            {
+                namn = Console.ReadLine();
+                
+                if (namn!="")
+                {
+                    list.Add(namn);
+                }
+            }
+            Console.WriteLine("Här är namnen sorterade alfabetiskt fast baklänges");
+            list.Sort();
+            list.Reverse();
+            foreach (String s in list) 
+            {
+                Console.WriteLine(s);
+
+            }
+
+        }
+    }
+}
